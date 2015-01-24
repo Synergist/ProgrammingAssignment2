4be52a126584f2af32265f0ef0181390b70a84b0
Pranav Singh <pranavsingh92@gmail.com>
2015-01-23 21:37:35 -0500

    implemented + documented the 2 functions. ready for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming - 010 | Coursera
+## Data Science Specialization - Johnss Hopkins University
+## Programming Assignment 2
+## Author: Pranav Singh
+## Date: 01/21/2015
+## Description: 
+##  Matrix inversion is typically a costly computatiion. If we expect to need a
+##  matrix's inverse multiple times, rather than computing it repeatedly, we can
+##  encompass a matrix in an object that also caches its inverse. The functions
+##  makeCacheMatrix and cacheSolve implement this feature.
 
-## Write a short comment describing this function
-
+## Creates a special "matrix" object (lets call it a CacheMatrix) that caches its inverse.
+## It returns a list with the following 4 functions as its elements:
+##      "set" : initializes a CacheMatrix object
+##      "get" : returns the regular matrix object stored in the CacheMatrix
+##      "setInverse" : caches the inverse of the matrix that the CacheMatrix object encompasses
+##      "getInverse" : returns the cached inverse 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inv) inverse <<- inv
+    getInverse <- function() inverse
+    list(set = set, get = get, getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Finds the inverse of x, which is assumed to be a CacheMatrix object that
+## encompasses an invertible matrix. If the inverse is already cached within the
+## CacheMatrix obbject, then it is simply returned without having to recompute it.
+## Otherwise, the inverse is computed with the standard solve() function, and is
+## then cached within the CacheMatrix x object for potential subsequent calls.
+## Note: it returns the inverse as a regular matrix object. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if (!is.null(inv)) return(inv)
+    else {
+        inv <- solve(x$get(), ...)
+        x$setInverse(inv)
+        return(inv)
+    }
 }
